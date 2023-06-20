754895e56612280c4d3c808a7969ec058f8bf97c
janicavc <crisostomo_janica@yahoo.com>
2023-06-20 09:24:20 -0700

    added state variable for paired cards

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 //*---- state variables ----*//
 let board; //array of 4c 4r
 let winner; // null = no winner, all cards have pairs = "Winner"
-
+let paired; // keep track of the cars that have fliped and are paired
 
 //*---- cached elements ----*//
 
